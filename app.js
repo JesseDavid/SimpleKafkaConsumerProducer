39ad61b72f440763b0038e2a3fd458153e5a9632
Jesse David <jesse.p.david@gmail.com>
2020-08-11 08:31:46 -0500

    removing superfluous code for now

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ const producer = new Kafka.Producer({
     }
 });
   
-//nforce's authentication to Salesforce org
-const org = nforce.createConnection({
-  clientId: process.env.CLIENTID,
-  clientSecret: process.env.CLIENTSECRET,
-  redirectUri: 'http://localhost:3000/oauth/_callback',
-  environment: process.env.ENVIRONMENT,
-  mode: 'single',
-});
+// //nforce's authentication to Salesforce org
+// const org = nforce.createConnection({
+//   clientId: process.env.CLIENTID,
+//   clientSecret: process.env.CLIENTSECRET,
+//   redirectUri: 'http://localhost:3000/oauth/_callback',
+//   environment: process.env.ENVIRONMENT,
+//   mode: 'single',
+// });
 
 return producer.init().then( ()=> {
     console.log('Producer Initiated');
@@ -40,33 +40,4 @@ return producer.init().then( ()=> {
         console.log(result);
     });
 
-});
-
-//Init the Producer, then auth the org, then subscribe to the channel, then on every message, victory.
-return producer.init().then(function(){
-    console.log('Producer Initiated');
-    org.authenticate({username: process.env.SFDCUSERNAME, password: process.env.SFDCPASSWORD}, function(err, resp){
-        if(!err){
-            console.log('SFDC Auth Connected');
-            console.log(`attempting client at ${org.oauth.instance_url}/cometd/45.0/`);
-            var fClient = new faye.Client(`${org.oauth.instance_url}/cometd/45.0/`);
-            fClient.setHeader('Authorization', 'OAuth ' + org.oauth.access_token);
-            fClient.subscribe(`${process.env.SF_CHANNEL}`, function(message){
-                console.log('Message Received');
-                //console.log(message.payload);
-                console.log(`Sending to Kafka Topic : ${process.env.KAFKA_TOPIC}`);
-                producer.send({
-                    topic:`${process.env.KAFKA_PREFIX}${process.env.KAFKA_TOPIC}`,
-                    partition:0,
-                    message:{
-                        value: JSON.stringify(message.payload)
-                    },
-                }).then(function(result){
-                    console.log(result);
-                });
-            });
-        } else{
-            console.error(err);
-        }
-    });
 });
