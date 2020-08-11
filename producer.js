b3db314ed4980e2b76c37076ada9683aee91dfee
Jesse David <jesse.p.david@gmail.com>
2020-08-11 10:49:02 -0500

    updating message to be a case message

--- producer.js
+++ producer.js
@@ -20,19 +20,15 @@ const producer = new Kafka.Producer({
 
 const caseObj = {
     'userName': 'Maria Edmunsen',
-    'caseDescription': `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida lectus sit amet eleifend lobortis. Mauris quis metus eros. Aliquam tristique semper blandit. Suspendisse orci velit, feugiat eu feugiat convallis, imperdiet ac lorem. Morbi sollicitudin, nisl id scelerisque viverra, diam dui egestas sem, et sodales elit purus quis nunc. Etiam eros justo, viverra in sem sit amet, consectetur sodales lacus. Nam non aliquam sapien. Proin lacus sapien, mollis in urna sit amet, mattis sagittis eros. Nulla luctus dui a mollis ultricies. Sed finibus tincidunt volutpat. In tincidunt ut ligula quis molestie. Maecenas vitae fringilla ante. Sed ullamcorper molestie risus, hendrerit fringilla tortor faucibus sed. Quisque vel feugiat mauris.
-
-                        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc laoreet, orci vel aliquam porttitor, nibh turpis consequat neque, id condimentum lacus turpis ac magna. Donec non iaculis ligula, vitae auctor ipsum. Maecenas vitae interdum ante. Nam eget magna egestas tellus pretium maximus vitae quis lorem. Aliquam condimentum vestibulum orci quis iaculis. Nam pulvinar turpis id euismod porta. Nam magna erat, vestibulum ac ullamcorper eu, mattis vitae dolor.`,
+    'caseDescription': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut gravida lectus sit amet eleifend lobortis. Mauris quis metus eros. Aliquam tristique semper blandit. Suspendisse orci velit, feugiat eu feugiat convallis, imperdiet ac lorem. Morbi sollicitudin, nisl id scelerisque viverra, diam dui egestas sem, et sodales elit purus quis nunc. Etiam eros justo, viverra in sem sit amet, consectetur sodales lacus. Nam non aliquam sapien. Proin lacus sapien, mollis in urna sit amet, mattis sagittis eros. Nulla luctus dui a mollis ultricies. Sed finibus tincidunt volutpat. In tincidunt ut ligula quis molestie. Maecenas vitae fringilla ante. Sed ullamcorper molestie risus, hendrerit fringilla tortor faucibus sed. Quisque vel feugiat mauris.',
     'time': Date.now().toLocaleString
 };
 
 return producer.init().then(() => {
     console.log('Producer Initiated');
 
-
     console.log(`Sending to Kafka Topic : ${process.env.KAFKA_TOPIC}`);
-
-    console.log(`Message sent: ${JSON.stringify(caseObj)}`)
+    console.log(`Message sent: ${JSON.stringify(caseObj)}`);
 
     producer.send({
         topic:`${process.env.KAFKA_PREFIX}${process.env.KAFKA_TOPIC}`,
@@ -41,6 +37,7 @@ return producer.init().then(() => {
             value: JSON.stringify(caseObj)
         },
     }).then((result) => {
+        console.log(`Message sent: ${JSON.stringify(caseObj)}`);
         console.log(result);
     });
 
